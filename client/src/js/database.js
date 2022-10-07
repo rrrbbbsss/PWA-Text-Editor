cae80c0b59a144b3c3e45aa13e81029df2302a82
Royce Strange <rrrbbbsss@ryzst.net>
2022-10-06 19:31:33 -0500

    add getDb and putDb functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,46 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // connect to db
+  const jateDb = await openDB("jate", 1);
+  // create a new transaction
+  const tx = jateDb.transaction("jate", "readwrite");
+  // open up desired object store
+  const store = tx.objectStore("jate");
+  // put data in db
+  const request = store.put({ id: 1, content: content });
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // get confirmation of request
+  const result = await request;
+  console.log("data saved", result);
+};
+
+export const getDb = async () => {
+  console.log("get from db");
+  // connect to db
+  const jateDb = await openDB("jate", 1);
+  // create a new transaction
+  const tx = jateDb.transaction("jate", "readonly");
+  // open up desired object store
+  const store = tx.objectStore("jate");
+  // get all data from db
+  const request = store.get(1);
+
+  // get confirmation of request
+  const result = await request;
+  return result.content;
+};
 
 initdb();
